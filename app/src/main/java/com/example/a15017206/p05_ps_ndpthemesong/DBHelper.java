5b9f9287767db4e3c6c1775971de815aa7d773f2
15017206 <15017206@myrp.edu.sg>
2017-05-18 14:52:33 +0800

    Commit 1

--- app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/DBHelper.java
+++ app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/DBHelper.java
@@ -29,7 +29,7 @@ public class DBHelper extends SQLiteOpenHelper{
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createNoteTableSql = " CREATE TABLE " + TABLE_NAME + "(" + SONG_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + SONG_TITLE + " TEXT, " + SINGER_NAME + " TEXT, " + YEAR_RELEASE +" TEXT, " + NO_OF_STARS + " INTEGER ) ";
+        String createNoteTableSql = " CREATE TABLE " + TABLE_NAME + "(" + SONG_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + SONG_TITLE + " TEXT, " + SINGER_NAME + " TEXT, " + YEAR_RELEASE +" INTEGER, " + NO_OF_STARS + " INTEGER ) ";
         db.execSQL(createNoteTableSql);
         Log.i("info", "created tables");
     }
