37bf3c9047678b700b51531e413999d49e854b82
15017206 <15017206@myrp.edu.sg>
2017-05-18 14:03:42 +0800

    Commit 1

---
+++ app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/DBHelper.java
@@ -0,0 +1,29 @@
+package com.example.a15017206.p05_ps_ndpthemesong;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by 15017206 on 18/05/2017.
+ */
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "ndpThemeSong.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/MainActivity.java
+++ app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.a15017206.p05_ps_ndpthemesong;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editSong, editSingerName, editYearRelease;
+    RadioGroup rg;
+    Button btnInsert, btnShowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editSong = (EditText) findViewById(R.id.editSong);
+        editSingerName = (EditText) findViewById(R.id.editSingerName);
+        editYearRelease = (EditText) findViewById(R.id.editYearRelease);
+        rg = (RadioGroup) findViewById(R.id.rg);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnShowList = (Button) findViewById(R.id.btnShowList);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String songTitle = editSong.getText().toString() ;
+                String songSinger = editSingerName.getText().toString();
+                String songYear = editYearRelease.getText().toString();
+
+DBHelper dbh = new DBHelper(MainActivity.this);
+
+            }
+        });
+
     }
 }
