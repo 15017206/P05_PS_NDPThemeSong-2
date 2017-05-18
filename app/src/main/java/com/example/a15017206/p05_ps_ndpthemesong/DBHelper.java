be7debf96c2a7acbf08e1a54acce33cde6ef83c0
15017206 <15017206@myrp.edu.sg>
2017-05-18 14:36:36 +0800

    Commit 1

--- app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/DBHelper.java
+++ app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/DBHelper.java
@@ -1,8 +1,10 @@
 package com.example.a15017206.p05_ps_ndpthemesong;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by 15017206 on 18/05/2017.
@@ -13,17 +15,46 @@ public class DBHelper extends SQLiteOpenHelper{
     private static final String DATABASE_NAME = "ndpThemeSong.db";
     private static final int DATABASE_VERSION = 1;
 
+    private static final String TABLE_NAME = "ndp";
+
+    //creating column names
+    private static final String SONG_ID = "song_id";
+    private static final String SONG_TITLE = "song_title";
+    private static final String SINGER_NAME = "singer_name";
+    private static final String YEAR_RELEASE = "year_released";
+    private static final String NO_OF_STARS = "no_of_stars";
+
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String createNoteTableSql = "CREATE TABLE " + TABLE_NAME + "(" + SONG_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + SONG_TITLE + "TEXT" + SINGER_NAME + "TEXT" + YEAR_RELEASE +"TEXT" + NO_OF_STARS + " INTEGER ) ";
+        db.execSQL(createNoteTableSql);
+        Log.i("info", "created tables");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public long insertSong(String songTitle, String singerName, int yearReleased, int noOfStars) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(SONG_TITLE, songTitle);
+        values.put(SINGER_NAME, singerName);
+        values.put(YEAR_RELEASE, yearReleased);
+        values.put(NO_OF_STARS, noOfStars);
+        long result = db.insert(TABLE_NAME, null, values);
+
+        if (result == -1) {
+            Log.d("DBHelper", "Insert failed");
+        }
+
+        db.close();
+        Log.d("SQL Insert", "" + result); //id returned, shouldn’t be -1
+        return result;
     }
 }
--- app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/MainActivity.java
+++ app/src/main/java/com/example/a15017206/p05_ps_ndpthemesong/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.a15017206.p05_ps_ndpthemesong;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editSong, editSingerName, editYearRelease;
     RadioGroup rg;
     Button btnInsert, btnShowList;
+    TextView debugText;
+
+    int starChosenNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +30,45 @@ public class MainActivity extends AppCompatActivity {
         rg = (RadioGroup) findViewById(R.id.rg);
         btnInsert = (Button) findViewById(R.id.btnInsert);
         btnShowList = (Button) findViewById(R.id.btnShowList);
+        debugText = (TextView) findViewById(R.id.debugText);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if (checkedId == R.id.radioButton) {
+                    starChosenNumber = 5;
+                } else if (checkedId == R.id.radioButton2) {
+                    starChosenNumber = 4;
+                } else if (checkedId == R.id.radioButton3) {
+                    starChosenNumber = 3;
+                } else if (checkedId == R.id.radioButton4) {
+                    starChosenNumber = 2;
+                } else if (checkedId == R.id.radioButton5) {
+                    starChosenNumber = 1;
+                }
+                debugText.setText(starChosenNumber + "");
+            }
+        });
 
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String songTitle = editSong.getText().toString() ;
-                String songSinger = editSingerName.getText().toString();
-                String songYear = editYearRelease.getText().toString();
 
-DBHelper dbh = new DBHelper(MainActivity.this);
+
+                String songTitle = editSong.getText().toString();
+                String songSinger = editSingerName.getText().toString();
+                int songYear = Integer.parseInt(editYearRelease.getText().toString());
+                int noOfStars = starChosenNumber;
+
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                debugText.setText(songTitle + songSinger + songYear + noOfStars + "");
+                long row_affected = dbh.insertSong(songTitle, songSinger, songYear, noOfStars);
+                dbh.close();
+
+                if (row_affected != -1) {
+                    Toast.makeText(MainActivity.this, "Insert successful",
+                            Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
